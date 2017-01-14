e34eef33af0fe081db6dcc94399a7d25e5168cfc
BenEmdon <benjaminemdon@gmail.com>
2017-01-13 21:10:31 -0500

    Added completed app

---
+++ public/index.js
@@ -0,0 +1,132 @@
+/**
+  Benjamin Emdon 100955811
+
+  @desc This script manages the DOM of index.html and interactes with the ChatRoom server using socket.io
+
+  Notes:
+    * arrow functions used `() => {...}` (ES6)
+    * ES6 object creation shortand
+*/
+
+const ENTER_EVENT = 13; // constant to check the enter key event
+
+$(document).ready(() => {
+
+  // MARK: - document load script
+
+  const username = prompt('Enter username: ') || 'User';
+
+  const socket = io(); // connect to the server that sent this page
+
+  // MARK: - socket event handlers
+
+  /**
+    @desc on 'connection' io event -> enter emit 'intro' event with username
+  */
+  socket.on('connect', () => {
+    socket.emit('intro', username);
+  });
+
+  /**
+    @desc on 'message' io event -> append the message to the text in the chatLog
+    @param message - a message broadcasted to the chatLog
+  */
+  socket.on('message', message => {
+    $('#chatLog').append(`${message}\n`);
+    scrolToBottom();
+  });
+
+  /**
+    @desc on 'userList' io event -> empty userList element and re-add all new <li> elements with jQuery event handlers
+    @param users - a list of users identified by their username
+  */
+  socket.on('userList', function(users) {
+    $('#userList').empty();
+    users.forEach(user => {
+      const listItem = $(`<li>${user}</ul>`);
+
+      /**
+        @desc on 'double click' -> start a private message conversation based on the username selected
+      */
+      listItem.dblclick(function() {
+        const userToMessage = $(this).text();
+        const displayMessage = `Private message to ${userToMessage}:`;
+        privateMessage(displayMessage, userToMessage);
+      });
+
+      /**
+        @desc on 'click + ctrlKey' or 'click + shiftKey' -> emit to the server with the username on the 'blockUser' event
+        @param ev - the event data on the click event
+      */
+      listItem.click(function(ev) {
+        if (ev.ctrlKey || ev.shiftKey) { // use of shiftKey -> because on Mac the control key is a meta key
+          const user = $(this).text();
+          socket.emit('blockUser', user);
+        }
+      });
+      $('#userList').append(listItem);
+    });
+  });
+
+  /**
+    @desc on 'privateMessage' -> enter a private message chat and display the message sent
+    @param data - an object containing the username and message of a private message
+  */
+  socket.on('privateMessage', data => {
+    privateMessage(data.message, data.username);
+  });
+
+  /**
+    @desc on 'blockUser' -> alert the user of a block status
+    @param message - the message to display on the alert
+  */
+  socket.on('blockUser', message => {
+    alert(message);
+  });
+
+  // MARK: - jquery event handlers
+
+  /**
+    @desc on 'enterKey' while the textInput is focused -> add message to the chatLog and emit the message on the 'message' event
+    @param ev - the event data on the kepress event
+  */
+  $('#inputText').keypress(function(ev) {
+    if(ev.which === ENTER_EVENT) {
+      socket.emit('message', $(this).val());
+      ev.preventDefault();
+      $('#chatLog').append(`${(new Date()).toLocaleTimeString()}, ${username}: ${$(this).val()}\n`);
+      $(this).val(''); // empty input are afterwards
+      scrolToBottom();
+    }
+  });
+
+  // MARK: - helper functions
+
+  /**
+    @desc makes the chat log scroll to the bottom
+  */
+  function scrolToBottom() {
+    $('#chatLog').scrollTop = $('#chatLog').scrollHeight;
+  }
+
+  /**
+    @desc promts the user with a message and asks for a message to reply/send on the 'privateMessage'
+    @param displayMessage - the message to display to the user on the prompt
+    @param username - the user identified by their name to send the message to
+  */
+  function privateMessage(displayMessage, username) {
+    const message = prompt(displayMessage);
+    console.log(message);
+    if (message) {
+      const data = { // ES6 object creation shortand
+        username,
+        message
+      }
+      socket.emit('privateMessage', data);
+    }
+  }
+
+  // MARK: - post event listener run script
+
+  $('#inputText').focus(); // focus the text input on document load
+});
---
+++ server.js
@@ -0,0 +1,178 @@
+/**
+  Benjamin Emdon 100955811
+
+  @desc This is a node scokect.io server that handles the ChatRoom app
+*/
+
+const http = require('http').createServer(handler);
+const io = require('socket.io')(http);
+const fs = require('fs');
+const url = require('url');
+const mime = require('mime-types');
+const ROOT = './public';
+const INDEX = 'index.html'
+
+http.listen(2406);
+
+console.log('Chat server listening on port 2406');
+
+/**
+  @desc handles HTTP requests
+  @param req - the request
+  @param res - the response
+*/
+function handler(req, res) {
+  const filename = ROOT + url.parse(req.url).pathname;
+  fs.stat(filename, (err, stats) => {
+    if (err) {
+      respondErr(err);
+    } else if (stats.isDirectory()) { // if directory serve up index.html
+      fs.readFile(filename + INDEX, (err, data) => {
+        if (err) {
+          respondErr(err);
+        } else {
+          respond(200, data, mime.lookup(filename + INDEX));
+        }
+      });
+    } else {
+      fs.readFile(filename, (err, data) => {
+        if (err) {
+          respondErr(err);
+        } else {
+          respond(200, data, mime.lookup(filename));
+        }
+      });
+    }
+  });
+
+  /**
+    @desc responds with the appropriate error given js error
+    @param err - the js error
+  */
+  function respondErr(err) {
+		console.log('Handling error: ', err);
+		if (err.code === 'ENOENT') {
+			serve404();
+		} else {
+			respond(500, err.message, null);
+		}
+	}
+
+  /**
+    @desc serves up a 404 page
+  */
+  function serve404() {
+		fs.readFile(ROOT + '/404.html', 'utf8', (err, data) => { // arrow functions (ES6)
+			if (err) {
+        respond(500, err.message, null);
+      } else {
+        respond(404, data, mime.lookup(filename));
+      }
+		});
+	}
+
+  /**
+    @desc responds with the given statusCode, data, and content-type
+    @param code - the statusCode
+    @param data - the data in some HTTP compatable form
+    @param contentType - the content-type to display the data as
+  */
+  function respond(code, data, contentType) {
+    res.writeHead(code, {'content-type': contentType || 'text/html'});
+    res.end(data);
+  }
+}
+
+// MARK: - socket.io events
+
+let clients = []; // list to store the clients in
+
+/**
+  @desc on 'connection' -> when a scoket connects
+  @param socket - the socket object
+*/
+io.on('connection', socket => {
+
+  /**
+    @desc on 'intro' -> register the socket as a cient and attach its username to it
+    @param username - the username to attach to the socket
+  */
+	socket.on('intro', username => {
+		clients.push(socket);
+		socket.username = username;
+    socket.blockedUsers = [];
+    // let everyone but the socket know that the socket has joined
+		socket.broadcast.emit('message', `${timestamp()}: ${socket.username} has entered the chatroom.`);
+    console.log(`${timestamp()}: ${socket.username} has entered the chatroom.`);
+    // welcome socket
+		socket.emit('message', `Welcome, ${socket.username}.`);
+    // notify everyone of the updated userList
+    io.emit('userList', clients.map(client => client.username)); // Arrow functions (ES6) + Higher order function Map
+	});
+
+  /**
+    @desc on 'message' -> broadcast a message to all the sockets
+    @param message - the message to be broadcasted
+  */
+	socket.on('message', message => {
+		socket.broadcast.emit('message',`${timestamp()}, ${socket.username}: ${message}`);
+    console.log(`${timestamp()}, ${socket.username}: ${message}`);
+	});
+
+  /**
+    @desc on 'disconnect' -> remove the socket from the client list and broadcast it's exit
+  */
+	socket.on('disconnect', () => {
+		console.log(`${socket.username} disconnected`);
+		io.emit('message', `${timestamp()}: ${socket.username} disconnected.`);
+		clients = clients.filter(element => element !== socket); // Arrow functions (ES6) + Higher order function Filter
+    io.emit('userList', clients.map(client => client.username));  // Arrow functions (ES6) + Higher order function Map
+	});
+
+  /**
+    @desc on 'privateMessage' -> relay message data to another socket (but only that socket)
+    @param data - contains the username identifying the recieveing socket and the message to send
+  */
+  socket.on('privateMessage', data => {
+    // Find first clients that share the same username.
+    const recievingUser = clients.find(client => client.username === data.username);
+
+    if (recievingUser !== undefined) {
+      if (recievingUser.blockedUsers.find(username => username === socket.username) === undefined) {
+        const newData = {
+          username: socket.username,
+          message: `${timestamp()}, private message from ${socket.username}:\n${data.message}`
+        }
+        // emit message only to that socket
+        recievingUser.emit('privateMessage', newData);
+        console.log(`Private message sent from ${socket.username} to ${data.username}`);
+      }
+    }
+  });
+
+  /**
+    @desc on 'blockUser' -> add a username to the sockets blockedUser list
+    @param username - username to be added to the blockedUser list
+  */
+  socket.on('blockUser', username => {
+    const blockedUser = socket.blockedUsers.find(user => user === username);
+    if (blockedUser === undefined) { // user isnt already blocked -> block user
+      socket.blockedUsers.push(username);
+      socket.emit('blockUser', `Blocked user: ${username}`);
+      console.log(`${socket.username} blocked ${username}`);
+    } else { // user is already blocked -> unblock user
+      socket.blockedUsers = socket.blockedUsers.filter(user => user !== username); // instead of splice(index, length), filter works cleaner with a similar runtime
+      socket.emit('blockUser', `Unblocked user: ${username}`);
+      console.log(`${socket.username} unblocked ${username}`);
+    }
+  });
+});
+
+// MARK: - Helper functions
+
+/**
+  @desc returns a time sting in the format [h:m:s a](ex. 4:41:35 PM)
+*/
+function timestamp() {
+	return new Date().toLocaleTimeString();
+}
